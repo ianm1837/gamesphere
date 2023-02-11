3e04d6c1df36c413476b0bed87d5fe8a05fb54d0
cridder <cjridder@gmail.com>
2023-02-11 12:41:45 -0800

    added folders, files, installs for 2fa

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const express = require('express');
 const session = require('express-session');
 const expressHandlebars = require('express-handlebars');
 
+// added by chris ridder for 2fa - 20230211123852
+// >
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
+const speakeasy = require("speakeasy");
+const JsonDB = require("node-json-db").JsonDB;
+const Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
+// The second argument is used to tell the DB to save after each push
+// If you put false, you'll have to call the save() method.
+// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
+// The last argument is the separator. By default it's slash (/)
+var db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/2fa", (req,res) => {
+  res.json({ message: "Welcome to the two factor authentication exmaple" })
+});
+//  <
+// added by chris ridder for 2fa - 20230211123852
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
