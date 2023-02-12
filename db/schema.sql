9f132f85cb95bca0a33032bb2ac05b90b55e29d3
ESTEVAN DADDY <enunez391@gmail.com>
2023-02-11 21:35:12 -0800

    inserted seeds for test

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,12 @@ CREATE DATABASE burp_db;
 
 USE burp_db;
 
+DROP TABLE IF EXISTS user;
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
     passcode VARCHAR(30) NOT NULL UNIQUE,
-    twoFactor VARCHAR(30) NOT NULL
+    twoFactor INT NOT NULL
 );
 
 CREATE TABLE posts (
@@ -27,36 +28,4 @@ CREATE TABLE comments (
     FOREIGN KEY (user_id)
     REFERENCES user (id),
     content TEXT NOT NULL
-=======
-
-CREATE TABLE user (
-    prim_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    passcode VARCHAR(30) NOT NULL,
-    twoFactor VARCHAR(30)  NOT NULL
-);
-
-CREATE TABLE posts (
-    prim_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (prim_id) 
-    REFERENCES user(prim_id),
-    
-    user_id PRIMARY KEY VARCHAR (30),
-    date_added ,
-    title VARCHAR (30),
-    content VARCHAR(99)
-);
-
-CREATE TABLE comments (
-    prim_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (prim_id) 
-    REFERENCES user(prim_id),
-    date_added ,
-
-    user_id 
-    FOREIGN KEY (user_id) 
-    REFERENCES posts(user_id)
-    VARCHAR(30),
-    post_id VARCHAR(30),
-    content VARCHAR(99)
 );
