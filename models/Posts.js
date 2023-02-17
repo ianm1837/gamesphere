49860dc4fc8657a56fb68a25d1f1700d88b794ae
ESTEVAN DADDY <enunez391@gmail.com>
2023-02-16 20:15:27 -0800

    added postsJS file

---
+++ models/Posts.js
@@ -0,0 +1,46 @@
+const {Model, DataTypes} = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: FALSE,
+            autoIncrement: TRUE,
+            primaryKey: TRUE,
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            allowNull: FALSE,
+            references: {
+                model:'user',
+                key: 'id',
+            },
+        },
+        timestamp: {
+            type: DataTypes.DATE,
+            allowNull: FALSE,
+            defaultVal: DataTypes.NOW,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: FALSE,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: FALSE,
+        },
+    },
+    {
+        sequelize, 
+        timestamps: FALSE,
+        freezeTableName: TRUE,
+        underscored: TRUE,
+        modelName: 'posts',
+    }    
+);
+
+module.exports = Posts; 
