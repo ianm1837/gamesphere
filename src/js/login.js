38aaf30268a3196ad2979084b912ca76d8890cce
ianm1837 <ian@iwm.dev>
2023-02-25 17:06:13 -0800

    fixed bugs with login and account creation

--- src/js/login.js
+++ src/js/login.js
@@ -1,17 +1,17 @@
-const deliverToast = require("./make-toast");
+const deliverToast = require('./make-toast');
 
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
   //get the values from the form
-  const username = document.querySelector("#username-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   //send the data to the server
-  const response = await fetch("/user/api/login", {
-    method: "POST",
+  const response = await fetch('/user/api/login', {
+    method: 'POST',
     body: JSON.stringify({ username, password }),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 
   //check the response status
@@ -21,12 +21,14 @@ const loginFormHandler = async (event) => {
       document.location.replace(data.url);
     });
   } else {
-    deliverToast("Unknown server error");
+    response.json().then((data) => {
+      deliverToast(data);
+    });
   }
 };
 
 // Check if the login form exists and add the event listener
-let loginForm = document.querySelector("#login-form") !== null;
+let loginForm = document.querySelector('#login-form') !== null;
 if (loginForm) {
-  document.querySelector("#login-form").addEventListener("submit", loginFormHandler);
+  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
 }
